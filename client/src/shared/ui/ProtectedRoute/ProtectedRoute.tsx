617c973d65a4c346a0ac36ec4bdb5d77c39f6aed
snow482 <a.dolin125@gmail.com>
2024-12-01 16:05:46 +0300

    test

--- client/src/shared/ui/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/shared/ui/ProtectedRoute/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { CLIENT_ROUTES } from "@/app/router";
 import { useAppSelector } from "@/shared/hooks/rtkHooks";
 import { ReactNode } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom"; 
 
 export function ProtectedRoute({ children }: { children: ReactNode }):
   | JSX.Element
